61050cc575d90193a53069bc25d5dc1467a0154d
pverma1 <pverma1@GUR00455316L>
2025-06-17 10:55:11 +0530

    Add Blockcollection queue initial commit

---
+++ Producer_Consumer/Producer_Consumer/BlockingConcurrentQueue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer_Consumer
+{
+    internal class BlockingConcurrentQueue<T> : IProducerConsumerCollection<T>
+
+    {
+        ConcurrentQueue<T> Queue = new();
+        private static readonly object Locker = new object();
+        CancellationToken cancellationToken;
+        public BlockingConcurrentQueue(int count, CancellationToken cancellationToken)
+        {
+            this.Count = count;
+            this.cancellationToken = cancellationToken;
+        }
+        public int Count { get; set; }
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void CopyTo(T[] array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryAdd(T item)
+        {
+            lock (Locker)
+            {
+                while (Queue.Count() >= Count)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    Monitor.Wait(Locker);
+                }
+                Queue.Enqueue(item);
+                Monitor.PulseAll(Locker);
+                return true;
+
+
+                //throw new NotImplementedException();
+            }
+        }
+
+        public bool TryTake([MaybeNullWhen(false)] out T item)
+        {
+            lock (Locker)
+            {
+                while (Queue.Count == 0)
+                {
+                    // if (_isAddingCompleted)
+                    //  throw new InvalidOperationException("No more items to take, and adding is completed.");
+
+                    cancellationToken.ThrowIfCancellationRequested();
+                    Monitor.Wait(Locker);
+                }
+                Queue.TryDequeue(out item);
+                Monitor.PulseAll(Locker);
+                return true;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Producer_Consumer/Producer_Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Producer_Consumer;
+
+Console.WriteLine("Hello, World!");
+int item = 0;
+CancellationTokenSource cts = new CancellationTokenSource();
+BlockingConcurrentQueue<int> queue = new(5,cts.Token);
+Thread t1 = new Thread(()=>TakeItem(out item));
+Thread t2 = new Thread(() => insertItem(5));
+t1.Start();
+Console.WriteLine("Taken item is " + item);
+t2.Start();
+
+Thread.Sleep(1000);
+
+Console.ReadLine();
+void TakeItem(out int item)
+{
+    queue.TryTake(out item);
+    Console.WriteLine("Taken item is " + item);
+}
+void insertItem(int item)
+{
+    queue.TryAdd(item);
+}
+
+    
+
+
+
+
